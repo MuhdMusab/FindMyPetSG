d1e5bd0fbfdf3351edf6e0b26ba486b6e844ef83
MuhdMusab <musab653@gmail.com>
2022-05-08 21:15:54 +0800

    Updated Sign-Up page

---
+++ lib/views/sign_up.dart
@@ -0,0 +1,116 @@
+import 'package:find_my_pet_sg/widgets/widget.dart';
+import "package:flutter/material.dart";
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: appBarMain(context),
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height - 50,
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration("username"),
+                  ),
+                  TextField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration("email"),
+                  ),
+                  TextField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration("password"),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Text(
+                        "Forgot Password?",
+                        style: simpleTextStyle(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC),
+                      ]),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text(
+                      "Sign up with Google",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an account? ", style: mediumTextStyle()),
+                        Text("Sign in",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 50,)
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
